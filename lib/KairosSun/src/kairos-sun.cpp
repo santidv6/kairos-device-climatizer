dea5e70927338a0f87afa4c67d85cb6f984782e9
santidv6 <santidv6@gmail.com>
2016-05-19 06:16:36 +0200

    primary kairos-sun files changes

--- lib/KairosSun/src/kairos-sun.cpp
+++ lib/KairosSun/src/kairos-sun.cpp
@@ -1,37 +1,43 @@
 #include "kairos-sun.hpp"
+#include "Wire.h"
+#include "RTClib.h"
+#include "TimerOne.h"
 
 KairosSun::KairosSun(const uint8_t relay) { this->relay = relay; }
 
 /**
  * Ajusta la hora y el minuto del día en el que se actiará el relé.
- * @param  hour   Hora del día en el que se activa el relé
- * @param  minute Minuto dentro de la hora en el que se activará el relé
+ * @param  hour_on   Hora del día en el que se activa el relé
+ * @param  minute_on Minuto dentro de la hora en el que se activará el relé
  * @return        Cero si no hay error, diferente de cero en caso contrario
  */
-uint8_t KairosSun::SetStartTime(uint8_t hour, uint8_t minute) {
+uint8_t KairosSun::SetSunrise(uint8_t hour_on, uint8_t minute_on) {
   // TODO
-
+	time_in_min_on = hour_on * 60 + minute_on;
   return 0;
 }
 
 /**
- * Ajusta la duración del estado ON del relé.
- * @param  interval Tiempo que está el relé en estado ON
- * @return          Cero si hay error, diferente de cero en caso contrario
+ * Ajusta la hora y el minuto del día en el que se desactivará el relé.
+ * @param  hour_off   Hora del día en el que se desactiva el relé
+ * @param  minute_off Minuto dentro de la hora en el que se desactivará el relé
+ * @return        Cero si no hay error, diferente de cero en caso contrario
  */
-uint8_t KairosSun::SetInterval(uint8_t interval) {
+uint8_t KairosSun::SetSunset(uint8_t hour_off, uint8_t minute_off) {
   // TODO
-
+	time_in_min_off = hour_off * 60 + minute_off;
   return 0;
 }
 
 /**
- * Comieza a contar el tiempo con las configuración actual
+ * Comieza a contar el tiempo con la configuración actual
  * @return Cero si hay error, diferente de cero en caso contrario
  */
 uint8_t KairosSun::Start() {
   // TODO
-
+	
+	
+	
   return 0;
 }
 
@@ -44,4 +50,8 @@ uint8_t KairosSun::Reload() {
   // TODO
 
   return 0;
+}
+
+void KairosSun::Interrupt() {
+	counter^=1;
 }
--- lib/KairosSun/src/kairos-sun.hpp
+++ lib/KairosSun/src/kairos-sun.hpp
@@ -5,14 +5,22 @@
  */
 class KairosSun {
   uint8_t relay;
-  uint8_t hour;
-  uint8_t minute;
-  uint8_t interval;
+  uint8_t hour_on;
+  uint8_t minute_on;
+  uint8_t hour_off;
+  uint8_t minute_off;
+  uint8_t time_in_min_on;
+  uint8_t time_in_min_off;
+  uint8_t SunState;
+  uint8_t counter = 0;
+  uint8_t previous_counter = 0;
+  uint8_t interrupt_period;
 
 public:
   KairosSun(const uint8_t relay);
-  uint8_t SetStartTime(uint8_t hour, uint8_t minute);
-  uint8_t SetInterval(uint8_t interval);
+  uint8_t SetSunrise(uint8_t hour_on, uint8_t minute_on);
+  uint8_t SetSunset(uint8_t hour_off, uint8_t minute_off);
   uint8_t Start();
   uint8_t Reload();
+  uint8_t Interrupt();
 };
